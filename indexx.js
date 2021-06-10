af4072811eab62d72766e3b8550891a6415e0427
Ravi <ravisinghxxxxl@gmail.com>
2021-06-10 10:27:53 +0300

    initial2

--- indexx.js
+++ indexx.js
@@ -1,4 +1,5 @@
 function updateMap() {
+  console.log("Updating map with real time data");
   fetch("/data.json")
     .then(response => response.json())
     .then(rsp => {
@@ -6,16 +7,21 @@ function updateMap() {
       rsp.data.forEach(element => {
         latitude = element.latitude;
         longitude = element.longitude;
-
+        cases = element.infected;
+         
+        if (cases > 255) {
+          color = "rgb(255, 0, 0)";
+        } else {
+          color = `rgb(${cases}, 0, 0)`;
+        }
         // Mark on the map
         new mapboxgl.Marker({
           draggable: false,
-          color: rgb(255, 0, 0)
+          color: color
         })
           .setLngLat([longitude, latitude])
           .addTo(map);
       });
     });
 }
-
-updateMap();
+setInterval(updateMap, 200000)
